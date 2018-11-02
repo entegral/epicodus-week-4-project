d381d3aa760d7606a55bd65a7c03e1ba31b85637
Rob <e2robby@gmail.com>
2018-11-02 12:05:35 -0700

    add ui constructor/prototype logic and some corresponding html

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,21 @@
         // Business Logic
 // ---------------------------------------------------------------------------
 
-function Order(name, delivery) {
+function Order() {
   this.list = [],
-  this.name = name,
+  this.name = "",
   this.address = "",
-  this.delivery = delivery,
+  this.delivery = false,
   this.subtotal = 0,
   this.total = 0
 };
 
-Order.prototype.addPizza = function (pizza) {
+Order.prototype.addPizza = function (veggieChoices, meatChoices, sauceChoice, crustChoice, sizeChoice) {
+  var pizza = new Pizza();
+  pizza.addToppings(veggieChoices, 1);
+  pizza.addToppings(meatChoices, 2);
+  pizza.addToppings(sauceChoice, 0.5);
+  pizza.addToppings(crustChoice, 1);
   this.list.push(pizza);
   this.subtotal += pizza.price;
 };
@@ -25,13 +30,14 @@ Order.prototype.calculateTotal = function () {
   });
 };
 
-function Pizza(size, toppings){
+function Pizza(){
   this.veggieOptions = ["sun dried tomatoes", "onion", "bell pepper", "olives", "mushrooms", "pineapple"],
   this.meatOptions = ["sausage", "pepperoni", "bacon", "chicken"],
   this.sauceOptions = ["marinara", "BBQ", "alfredo", "pesto", "buffalo"],
   this.crustOptions = ["thin", "stuffed", "deep dish", "regular"],
-  this.sizeOptions = ["small", "medium", "large", "family"]
-  this.userChoice = [],
+  this.sizeOptions = ["small", "medium", "large", "family"],
+  this.toppingChoices = [],
+  this.size = "",
   this.price = 0
 };
 
@@ -60,14 +66,65 @@ Pizza.prototype.setSize = function (sizeSelection) {
 
 // ---------------------------------------------------------------------------
         // User Interface Logic
-// ---------------------------------------------------------------------------
+// -------------------------------------------------------------------------
+
+function BuildPizzaPage(pizza){
+  this.veggieOptions = pizza.veggieOptions,
+  this.meatOptions = pizza.meatOptions,
+  this.sauceOptions = pizza.sauceOptions
+  this.crustOptions = pizza.crustOptions,
+  this.sizeOptions = pizza.sizeOptions
+}
+
+BuildPizzaPage.prototype.displayVeggies = function (veggieSelector) {
+  var veggiesToDisplay = "";
+  this.veggieOptions.forEach(function(topping){
+    veggiesToDisplay += "<div class='checkbox'><label><input type='checkbox' name='veggieOptions' value='" + topping + "'>" + topping + "</label></div>"
+  });
+  veggieSelector.html(veggiesToDisplay);
+}
+
+BuildPizzaPage.prototype.displayMeats = function (meatSelector) {
+  var meatsToDisplay = "";
+  this.meatOptions.forEach(function(topping){
+    meatsToDisplay += "<div class='checkbox'><label><input type='checkbox' name='meatOptions' value='" + topping + "'>" + topping + "</label></div>"
+  });
+  meatSelector.html(meatsToDisplay);
+}
+
+BuildPizzaPage.prototype.displaySauces = function (sauceSelector) {
+  var saucesToDisplay = "";
+  this.sauceOptions.forEach(function(topping){
+    saucesToDisplay += "<option value='" + topping + "'>" + topping + "</option>"
+  });
+  sauceSelector.html(saucesToDisplay);
+}
+
+BuildPizzaPage.prototype.displaySizes = function (sizeSelector) {
+  var sizesToDisplay = "";
+  this.sizeOptions.forEach(function(topping){
+    sizesToDisplay += "<option value='" + topping + "'>" + topping + "</option>"
+  });
+  sizeSelector.html(sizesToDisplay);
+}
+
+BuildPizzaPage.prototype.displayCrusts = function (crustSelector) {
+  var crustsToDisplay = "";
+  this.crustOptions.forEach(function(topping){
+    crustsToDisplay += "<option value='" + topping + "'>" + topping + "</option>"
+  });
+  crustSelector.html(crustsToDisplay);
+}
 
 
 $(function() {
 
   $("#buildPie").click(function(){
-    $("#intro").hide()
-    $(".build").show()
+    var order = new Order();
+    var buildPizzaPage = BuildPizzaPage(new Pizza());
+
+    $("#intro").hide();
+    $(".build").show();
 
 
   })
